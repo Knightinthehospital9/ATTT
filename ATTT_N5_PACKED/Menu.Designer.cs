f6d3271f10654312e757af0be3262100723c801b
Nam Nguyen <namdk2003@gmail.com>
2024-11-10 16:38:15 +0700

    Nam design lai anh

--- ATTT_N5_PACKED/Menu.Designer.cs
+++ ATTT_N5_PACKED/Menu.Designer.cs
@@ -114,10 +114,11 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 72);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 53);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1004, 551);
+            this.pictureBox1.Size = new System.Drawing.Size(1028, 582);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
